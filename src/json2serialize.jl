9f82ccc45542f55091c6bdf072c717b2bc500aa9
yakir12 <12.yakir@gmail.com>
2019-07-03 14:43:41 +0200

    convert json

--- src/json2serialize.jl
+++ src/json2serialize.jl
@@ -1,12 +1,26 @@
-using Databula, DungBase, JSON3, Serialization
+using DungBase, JSON3, Serialization, Dates
+
+for y in (DungBase, Dates), x in names(y, all = true)
+    T = getproperty(y, x)
+    if T isa DataType
+        JSON3.StructType(::Type{T}) = JSON3.Struct()
+    elseif T isa UnionAll
+        JSON3.StructType(::Type{<:T}) = JSON3.Struct()
+    end
+end
+
 
 file = joinpath(Databula.sourcefolder, "video.json")
 videos = open(file, "r") do io
     JSON3.read(io, Vector{Union{WholeVideo, FragmentedVideo, DisjointVideo}})
 end
+serialize(joinpath(Databula.sourcefolder, "video"), videos)
 
-file = joinpath(Databula.sourcefolder, "calibrations.json")
-videos = open(file, "r") do io
+#=file = joinpath(Databula.sourcefolder, "calibrations.json")
+calibrations = open(file, "r") do io
     JSON3.read(io, Vector{Calibration})
 end
+serialize(joinpath(Databula.sourcefolder, "calibration"), calibrations)=#
+
+rm(joinpath(Databula.sourcefolder, "calibrations.json"))
 
